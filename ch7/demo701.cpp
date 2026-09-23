4f196c4fa3c8fc9c1569783d2e825089110e0b4e
frank22202 <frank2007@gmail.com>
2026-09-23 14:51:16 -0400

    Add demo701.cpp with array initialization and output
    Initialize an array of integers and a fixed array of doubles. Output the first score from the scores array.

---
+++ ch7/demo701.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[100];
+    numbers[0] = 12;
+    numbers[1] = 2;
+
+    double scores[5] = {99.7, 87.12, 33.56};
+    cout << scores[0] << endl;
+    cout << scores[3];
+
+    int number1 = 12;
+    int number2 = 2;
+
+
+}
